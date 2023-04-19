33adcc731a9cb2107016e538beaf61ac0c633ef1
RyanHira <ryan.hira@hotmail.nl>
2023-04-19 10:22:17 +0200

    CrudPush

---
+++ Crud/Models/CourseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Crud.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public List<TeacherModel>? Teachers { get; set; }
+        public List<StudentModel>? Students { get; set; }
+    }
+}
